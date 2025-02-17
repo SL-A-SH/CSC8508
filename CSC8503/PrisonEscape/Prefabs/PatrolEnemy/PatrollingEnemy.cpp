64631944a408aa1e90c55d2d59d6ad706bb68f01
broden-bates <brodenbates@gmail.com>
2025-02-17 17:17:24 +0000

    Enemy patrol logic implemented

---
+++ CSC8503/PrisonEscape/Prefabs/PatrolEnemy/PatrollingEnemy.cpp
@@ -0,0 +1,11 @@
+#include "PatrollingEnemy.h"
+
+using namespace NCL;
+using namespace CSC8503;
+
+PatrollingEnemy::PatrollingEnemy() : PatrolEnemy(){
+
+}
+
+PatrollingEnemy::~PatrollingEnemy() {
+}
