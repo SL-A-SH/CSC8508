f783145cc950d820f9f4806d548bcde040342e2c
Labham Baheti <labham7@gmail.com>
2025-03-15 15:14:16 +0000

    Update ImGuiManager.cpp

--- CSC8503/PrisonEscape/Core/ImGuiManager.cpp
+++ CSC8503/PrisonEscape/Core/ImGuiManager.cpp
@@ -134,22 +134,20 @@ void ImGuiManager::DrawHeader(const std::string& title) {
 }
 
 void ImGuiManager::DrawButton(const std::string& label, const std::function<void()>& callback, float xPos, float yPos) {
-	ImVec2 windowSize = ImGui::GetWindowSize();
 
-	ImGui::PushFont(buttonFont);
 	// Set cursor position using the new x and y positions
-	ImGui::SetCursorPos(ImVec2(windowSize.x * xPos, windowSize.y * yPos));
 	//nEED TO FIND A WAY TO LOAD tEXTURE HERE THIS WON'T WORL
 	/*Texture* tex = GameBase::GetGameBase()->GetRenderer()->LoadTexture("button.png");
 	GLuint texID = ((OGLTexture*)tex)->GetObjectID();*/
+
+	ImVec2 windowSize = ImGui::GetWindowSize();
+	ImGui::PushFont(buttonFont);
+	ImGui::SetCursorPos(ImVec2(windowSize.x * xPos, windowSize.y * yPos));
 	if (ImGui::Button(label.c_str(), ImVec2(windowSize.x * BUTTON_WIDTH_RATIO, windowSize.y * BUTTON_HEIGHT_RATIO))) {
 		if (callback) {
 			callback();
 		}
 	}
-
-
-
 	ImGui::PopFont();
 }
 
