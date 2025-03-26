d6fd11725f48554b8f51d0a90f61a1952ee33f74
HaygenSee <c1014575@newcastle.ac.uk>
2025-03-26 00:04:50 +0000

    convertToJson.cs with enemy waypoints
    - Enemies can now be added with waypoints as its children gameobjects
    - When making enemies NAME them Enemy1, Enemy2 etc and give them the Enemy TAG
    - Waypoints give em Waypoint tags and ofc put under parent enemy

--- CSC8503/PrisonEscape/Scripts/Unity json/convertToJson.cs
+++ CSC8503/PrisonEscape/Scripts/Unity json/convertToJson.cs
@@ -31,10 +31,19 @@ public class GameObjectData
 }
 
 [System.Serializable]
+public class EnemyData
+{
+    public string name;
+    public Position position;
+    public List<Position> waypoints = new List<Position>();
+}
+
+[System.Serializable]
 public class LevelData
 {
     public int level = 1; // changable level number
     public List<GameObjectData> objects = new List<GameObjectData>();
+    public List<EnemyData> enemies = new List<EnemyData>();
 }
 
 public class convertToJson : MonoBehaviour
@@ -52,37 +61,59 @@ public class convertToJson : MonoBehaviour
         GameObject[] allObjects = FindObjectsOfType<GameObject>();
 
         int idCounter = 1;
-        foreach (GameObject obj in allObjects)
-        {
+        foreach (GameObject obj in allObjects) {
             if (!obj.activeInHierarchy) continue;
 
-            GameObjectData gameObjectData = new GameObjectData
-            {
-                id = idCounter++,
-                type = obj.tag != "Untagged" ? obj.tag : obj.name, 
-                position = new Position
-                {
-                    x = obj.transform.position.x,
-                    y = obj.transform.position.y,
-                    z = obj.transform.position.z
-                },
-                dimensions = new Dimensions
-                {
-                    x = obj.transform.localScale.x,
-                    y = obj.transform.localScale.y,
-                    z = obj.transform.localScale.z
-                },
-                orientation = new Orientation
-                {
-                    x = obj.transform.eulerAngles.x, 
-                    y = obj.transform.eulerAngles.y,
-                    z = obj.transform.eulerAngles.z
+            if (obj.CompareTag("Enemy")) {
+                EnemyData enemyData = new EnemyData {
+                    name = obj.name,
+                    position = new Position {
+                        x = obj.transform.position.x,
+                        y = obj.transform.position.y,
+                        z = obj.transform.position.z
+                    }
+                };
+
+                foreach (Transform child in obj.transform) {
+                    if (child.CompareTag("Waypoint")) {
+                        Vector3 worldPosition = child.TransformPoint(Vector3.zero);
+
+                        Position waypointPos = new Position {
+                            x = worldPosition.x,
+                            y = worldPosition.y,
+                            z = worldPosition.z,
+                        };
+
+                        enemyData.waypoints.Add(waypointPos);
+                    }
                 }
-            };
+                
+                levelData.enemies.Add(enemyData);
+            }
+            else if (!obj.CompareTag("Waypoint")) {
+                GameObjectData gameObjectData = new GameObjectData {
+                    id = idCounter++,
+                    type = obj.tag != "Untagged" ? obj.tag : obj.name, 
 
-            levelData.objects.Add(gameObjectData); 
+                    position = new Position {
+                        x = obj.transform.position.x,
+                        y = obj.transform.position.y,
+                        z = obj.transform.position.z
+                    },
+                    dimensions = new Dimensions {
+                        x = obj.transform.localScale.x,
+                        y = obj.transform.localScale.y,
+                        z = obj.transform.localScale.z
+                    },
+                    orientation = new Orientation {
+                        x = obj.transform.eulerAngles.x, 
+                        y = obj.transform.eulerAngles.y,
+                        z = obj.transform.eulerAngles.z
+                    }
+                };
+                levelData.objects.Add(gameObjectData); 
+            }
         }
-
         string json = JsonUtility.ToJson(levelData, true); 
         File.WriteAllText(Application.persistentDataPath + "/" + fileName, json); 
         Debug.Log("Level exported to " + Application.persistentDataPath + "/" + fileName);
